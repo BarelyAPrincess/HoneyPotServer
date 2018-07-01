6a06dc04aad36b572a7123d0205c82d9b190193a
Amelia DeWitt <theameliadewitt@gmail.com>
2018-07-01 00:55:43 -0500

    Fixed copyright header and made changes to how ContainerBase deals with exceptions

--- modules/HPNetworking/src/main/java/io/amelia/networking/ipc/IPC.java
+++ modules/HPNetworking/src/main/java/io/amelia/networking/ipc/IPC.java
@@ -2,7 +2,7 @@
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
  * <p>
- * Copyright (c) 2018 Amelia DeWitt <theameliadewitt@ameliadewitt.com>
+ * Copyright (c) 2018 Amelia DeWitt <me@ameliadewitt.com>
  * Copyright (c) 2018 Penoaks Publishing LLC <development@penoaks.com>
  * <p>
  * All Rights Reserved.
